c5f8291d912887b9acd1f23a93c4a53e22da121f
J-O-B <Jonathanmichaelobrien@gmail.com>
2021-03-06 21:32:43 +0000

    heroku issue

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ from flask import (
     Flask, render_template,
     redirect, request, session, url_for)
 from flask_pymongo import PyMongo
-from twitterBot import *
 if os.path.exists("env.py"):
     import env
 
@@ -20,7 +19,6 @@ mongo = PyMongo(app)
 def home():
     tweets = mongo.db.tweets.find().sort('time_posted', -1)
     count = mongo.db.tweets.count()
-    StdOutListener()
     return render_template("index.html", tweets=tweets, count=count)
 
 
@@ -28,10 +26,3 @@ if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
             debug=True)
-    auth = OAuthHandler(API_KEY, API_SECRET_KEY)
-    auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
-    # Listen for a stream
-    listen = StdOutListener()
-    stream = Stream(auth, listen)
-    # Filter the stream to tweets containing #theyThinkItsAllClover:
-    stream.filter(track=['#theyThinkItsAllClover'])
