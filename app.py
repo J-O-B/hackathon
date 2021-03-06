6cddcf410e02744f29724acbc8d8bfee98a16ac4
J-O-B <Jonathanmichaelobrien@gmail.com>
2021-03-06 19:32:50 +0000

    initial hackathon template for push

--- app.py
+++ app.py
@@ -3,9 +3,7 @@ from flask import (
     Flask, render_template,
     redirect, request, session, url_for)
 from flask_pymongo import PyMongo
-from datetime import *
-from bson.objectid import ObjectId
-from werkzeug.security import generate_password_hash, check_password_hash
+from twitterBot import *
 if os.path.exists("env.py"):
     import env
 
@@ -21,11 +19,20 @@ mongo = PyMongo(app)
 
 @app.route("/", methods=["GET", "POST"])
 def home():
-    tweets = mongo.db.tweets.find()
-    return render_template("index.html", tweets=tweets)
+    tweets = mongo.db.tweets.find().sort('time_posted', -1)
+    count = mongo.db.tweets.count()
+    StdOutListener()
+    return render_template("index.html", tweets=tweets, count=count)
 
 
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
             debug=True)
+    auth = OAuthHandler(API_KEY, API_SECRET_KEY)
+    auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+    # Listen for a stream
+    listen = StdOutListener()
+    stream = Stream(auth, listen)
+    # Filter the stream to tweets containing #theyThinkItsAllClover:
+    stream.filter(track=['#theyThinkItsAllClover'])
--- static/script.js
+++ static/script.js
@@ -1,3 +1,9 @@
+$( document ).ready(function() {
+    $('.tweetCard').hide();
+    $('#coordinates').hide();
+    $('.lat').hide();
+    $('.long').hide();
+});
 var map = L.map('map').setView([53.34, -6.26], 4);
 
 L.tileLayer('https://api.maptiler.com/maps/hybrid/{z}/{x}/{y}.jpg?key=ZK2Os6eoqo86I37zYlQO', {
@@ -10,6 +16,72 @@ var ourIcon = L.icon({
     iconAnchor: [20,30],
 });
 
-L.marker([53.345, -6.265],{icon:ourIcon}).addTo(map)
+//Dublin
+L.marker([53.349804, -6.260310],{icon:ourIcon}).addTo(map)
     .bindPopup('<img src="https://i.imgur.com/8ICyCUc.jpg" alt="userPhoto" class="userPic"><br>Welcome To They Think Its All Clover!<br>Scroll Out To View St.Patricks Tweets From<br>Around The World')
     .openPopup();
+    /*
+//London
+let londonlat = 51.50
+let londonlong = 0.13
+L.marker([londonlat, londonlong],{icon:ourIcon}).addTo(map)
+    .bindPopup('<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d6/St_Patrick%27s_Day_-_Trafalgar_Square_March_2006.jpg/220px-St_Patrick%27s_Day_-_Trafalgar_Square_March_2006.jpg" alt="userPhoto" class="userPic"><br>Trafalgar Square');
+//Floriana Malta
+L.marker([35.89, 14.50],{icon:ourIcon}).addTo(map)
+    .bindPopup('<img src="https://malta.intercontinental.com/wp-content/uploads/2018/03/St-Patricks-Day-Celebrations-In-Valletta.jpg" alt="userPhoto" class="userPic"><br>Floriana Malta');
+//Moscow
+L.marker([55.75, 37.62],{icon:ourIcon}).addTo(map)
+    .bindPopup('<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/77/St_Patrick%27s_Day_2012_in_Moscow.jpg/220px-St_Patrick%27s_Day_2012_in_Moscow.jpg" alt="userPhoto" class="userPic"><br>Moscow');
+//Sarajevo
+L.marker([43.85, 18.41],{icon:ourIcon}).addTo(map)
+    .bindPopup('<img src="" alt="userPhoto" class="userPic"><br>Sarajevo');
+//Scotland
+L.marker([55.862, -4.251],{icon:ourIcon}).addTo(map)
+    .bindPopup('<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/76/Stpatricksdayfestivalcbridge.JPG/220px-Stpatricksdayfestivalcbridge.JPG" alt="userPhoto" class="userPic"><br>Scotland');
+//Lithuania
+L.marker([54.68, 25.28],{icon:ourIcon}).addTo(map)
+    .bindPopup('<img src="" alt="userPhoto" class="userPic"><br>Lithuania');
+//Tokyo
+L.marker([35.68, 139.76],{icon:ourIcon}).addTo(map)
+    .bindPopup('<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/aa/Saint_Patricks_Day_in_Motomachi_Yokohama.jpg/220px-Saint_Patricks_Day_in_Motomachi_Yokohama.jpg" alt="userPhoto" class="userPic"><br>Tokyo');
+//Cheonggyecheon
+L.marker([37.57, 126.97],{icon:ourIcon}).addTo(map)
+    .bindPopup('<img src="" alt="userPhoto" class="userPic"><br>Cheonggyecheon');
+//Selangor
+L.marker([3.07, 101.51],{icon:ourIcon}).addTo(map)
+    .bindPopup('<img src="" alt="userPhoto" class="userPic"><br>Selangor');*/
+
+var count = $("#coordinates").children().length;
+
+var i;
+for (i = 0; i < count; i++) {
+    $(this).css("background", "red");
+}
+$('#coordinates').each(function(){
+    marker();
+});
+function marker(){
+    $('#coordinates').each(function(){
+    let lat = $(this).find('.lat').val();
+    let long = $(this).find('.long').val();
+    let pic = $(this).find('.pic').val();
+    let message = $(this).find('.tweet').val();
+    let name = $(this).find('.name').val();
+
+    console.log(typeof lat)
+    console.log(typeof long)
+    L.marker([lat, long],{icon:ourIcon}).addTo(map)
+    .bindPopup(`<img src="${pic}" alt="userPhoto" class="userPic"><br><strong>${name}</strong><br><em>${message}</em>`);
+    });
+}
+
+
+$('#tweetBtn').click(function(){
+    $('.tweetdata').hide(1000);
+    $('.tweetCard').show(2000);
+})
+
+$('#backBtn').click(function(){
+    $('.tweetCard').hide(1000);
+    $('.tweetdata').show(2000);
+})
