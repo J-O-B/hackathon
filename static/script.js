e6e415403ddc9092f4fb7304c523d83ba438f053
J-O-B <Jonathanmichaelobrien@gmail.com>
2021-03-07 15:25:40 +0000

    working app

--- static/script.js
+++ static/script.js
@@ -18,63 +18,32 @@ L.tileLayer('https://api.maptiler.com/maps/hybrid/{z}/{x}/{y}.jpg?key=ZK2Os6eoqo
 
 var ourIcon = L.icon({
     iconUrl: "https://i.imgur.com/CbAdhGo.png",
-    iconSize: [35,45],
-    iconAnchor: [20,35],
+    iconSize: [30,40],
+    iconAnchor: [20,30],
 });
 
 //Dublin
 L.marker([53.349804, -6.260310],{icon:ourIcon}).addTo(map)
     .bindPopup('<img src="https://i.imgur.com/8ICyCUc.jpg" alt="userPhoto" class="userPic"><br>Welcome To They Think Its All Clover!<br>Scroll Out To View St.Patricks Tweets From<br>Around The World')
     .openPopup();
-//London
-L.marker([51.50, 0.13],{icon:ourIcon}).addTo(map)
-    .bindPopup(`<img src="https://traveljee.com/wp-content/uploads/2016/03/St-Patricks-Day-London-600x399.jpg" alt="userPhoto class ="userPic"><br><strong>London</strong>`);
-//Floriana Malta
-L.marker([35.89, 14.50],{icon:ourIcon}).addTo(map)
-.bindPopup(`<img src="https://archive.lovinmalta.com/images/uploads/2019/03/_blogWide/28944159_10215694282820133_2026537199_o.jpg?mtime=20190317103738" alt="userPhoto class ="userPic"><br><strong>Floriana</strong>`);
-//Moscow
-L.marker([55.75, 37.62],{icon:ourIcon}).addTo(map)
-.bindPopup(`<img src="https://i.pinimg.com/originals/7c/2c/bb/7c2cbb56d8bf79d3b32835122db69bb5.jpg" alt="userPhoto class ="userPic"><br><strong>Moscow</strong>`);
-//Sarajevo
-L.marker([43.85, 18.41],{icon:ourIcon}).addTo(map)
-.bindPopup(`<img src="" alt="userPhoto class ="userPic"><br><strong>Sarajevo</strong>`);
-//Scotland
-L.marker([55.862, -4.251],{icon:ourIcon}).addTo(map)
-.bindPopup(`<img src="" alt="userPhoto class ="userPic"><br><strong>London</strong>`);
-//Lithuania
-L.marker([54.68, 25.28],{icon:ourIcon}).addTo(map)
-.bindPopup(`<img src="" alt="userPhoto class ="userPic"><br><strong>London</strong>`);
-//Tokyo
-L.marker([35.68, 139.76],{icon:ourIcon}).addTo(map)
-.bindPopup(`<img src="" alt="userPhoto class ="userPic"><br><strong>London</strong>`);
-//Cheonggyecheon
-L.marker([37.57, 126.97],{icon:ourIcon}).addTo(map)
-.bindPopup(`<img src="" alt="userPhoto class ="userPic"><br><strong>London</strong>`);
-//Selangor
-L.marker([3.07, 101.51],{icon:ourIcon}).addTo(map)
-.bindPopup(`<img src="" alt="userPhoto class ="userPic"><br><strong>London</strong>`);
 
-var count = $("#coordinates").children().length;
-
-var i;
-for (i = 0; i < count; i++) {
-    $(this).css("background", "red");
-}
 $('#coordinates').each(function(){
     marker();
 });
 function marker(){
     $('#coordinates').each(function(){
-    let lat = $(this).find('.lat').val();
-    let long = $(this).find('.long').val();
-    let pic = $(this).find('.pic').val();
-    let message = $(this).find('.tweet').val();
-    let name = $(this).find('.name').val();
+        $('.coordinates').each(function(){
+            let lat = $(this).find('.lat').val();
+            let long = $(this).find('.long').val();
+            let pic = $(this).find('.pic').val();
+            let message = $(this).find('.tweet').val();
+            let name = $(this).find('.name').val();
 
-    console.log(typeof lat)
-    console.log(typeof long)
-    L.marker([lat, long],{icon:ourIcon}).addTo(map)
-    .bindPopup(`<img src="${pic}" alt="userPhoto" class="userPic"><br><strong>${name}</strong><br><em>${message}</em>`);
+            console.log(typeof lat)
+            console.log(typeof long)
+            L.marker([lat, long],{icon:ourIcon}).addTo(map)
+            .bindPopup(`<img src="${pic}" alt="userPhoto" class="userPic"><br><strong>${name}</strong><br><em>${message}</em>`);
+        });
     });
 }
 
